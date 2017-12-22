702f9f9347eded65d3a2483c78212685f1a4a91f
Akhyar Amarullah <akhyrul@gmail.com>
2017-12-22 17:37:48 +0700

    initial ci

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 REPO_NAME = 'chickenzord/ghstatus'
-VERSION = '0.1.0'
+VERSION = '0.0.0'
 ARCHIVE_URL = 'https://github.com/%s/archive/v%s.tar.gz' % (REPO_NAME, VERSION)
 
 setup(
