c19eaecc9be1e806f26b0cd2a9c08451b5a7d6b7
Akhyar Amarullah <akhyrul@gmail.com>
2017-12-27 21:26:49 +0700

    add exec command

--- src/ghstatus/cli.py
+++ src/ghstatus/cli.py
@@ -1,15 +1,29 @@
 import click
+import logging
 import requests
+import sys
 try:
     import simplejson as json
 except ImportError:
     import json
-from sh import git
+from sh import git, Command, ErrorReturnCode, CommandNotFound
 
 
 DEFAULT_GITHUB_URL = 'https://api.github.com'
 DEFAULT_CONTEXT = 'default'
+DEFAULT_SUCCESS_MESSAGE = 'Tests pass!'
+DEFAULT_PENDING_MESSAGE = 'Tests in progress'
+DEFAULT_FAILURE_MESSAGE = 'Tests failed'
+DEFAULT_ERROR_MESSAGE = 'Cannot run tests'
 STATES = ['error', 'failure', 'pending', 'success']
+
+is_interactive = sys.__stdin__.isatty()
+log = logging.getLogger('ghstatus')
+handler = logging.StreamHandler()
+formatter = logging.Formatter('[%(name)s:%(levelname)s] %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
 
 
 def lstrip(text, prefix):
@@ -51,13 +65,54 @@ def main(ctx, base_url, username, password, repo, sha):
     ctx.obj['sha'] = sha
 
 
-@main.command()
+@main.command('exec')
 @click.option('--context', default=DEFAULT_CONTEXT, show_default=True)
-@click.option('--description')
-@click.option('--target-url')
+@click.option('--target-url', help='')
+@click.option('--pending', default=DEFAULT_PENDING_MESSAGE,
+              help='Pending message.')
+@click.option('--success', default=DEFAULT_SUCCESS_MESSAGE,
+              help='Success message.')
+@click.option('--failure', default=DEFAULT_FAILURE_MESSAGE,
+              help='Failure message.')
+@click.option('--error', default=DEFAULT_ERROR_MESSAGE,
+              help='Error message.')
+@click.argument('command', nargs=-1)
+@click.pass_context
+def exe(ctx, context, target_url, pending, success, failure, error, command):
+    def _status_set(state, description):
+        log.info('%s -> %s', context, state)
+        ctx.invoke(status_set, context=context, target_url=target_url,
+                   state=state, description=description, silent=True)
+
+    try:
+        _status_set('pending', pending)
+        cmd = Command(command[0])
+        cmd = cmd.bake(command[1:]) if len(command) > 1 else cmd
+        cmd(_fg=True)
+        _status_set('success', success)
+        exit(0)
+    except ErrorReturnCode as e:
+        log.error('Command failed with exit code: %d', e.exit_code)
+        _status_set('failure', failure)
+    except CommandNotFound as e:
+        log.error('Command not found: %s', e)
+        _status_set('error', error)
+    except Exception as e:
+        log.error(e, exc_info=True)
+        _status_set('error', error)
+
+    exit(1)
+
+
+@main.command('set')
+@click.option('--context', default=DEFAULT_CONTEXT, show_default=True)
+@click.option('--description', help='Descriptive status message.')
+@click.option('--target-url', help='URL to the status.')
+@click.option('--silent', help='URL to the status.',
+              is_flag=True, default=False)
 @click.argument('state', type=click.Choice(STATES))
 @click.pass_context
-def set(ctx, state, context, description, target_url):
+def status_set(ctx, state, context, description, target_url, silent):
     base_url = ctx.obj.get('base_url')
     username = ctx.obj.get('username')
     password = ctx.obj.get('password')
@@ -72,17 +127,20 @@ def set(ctx, state, context, description, target_url):
         'target_url': target_url,
     }
 
-    print(locals())
-
     r = requests.post(status_url, json=payload, auth=(username, password))
-    click.echo(json.dumps(r.json(), indent=2))
+    if not silent:
+        result = json.dumps(r.json(), indent=2)
+        print(result)
+
+    if r.status_code != 200 and not silent:
+        exit(1)
 
 
-@main.command()
+@main.command('get')
 @click.option('--context', default=DEFAULT_CONTEXT, show_default=True)
 @click.option('json_output', '--json/--no-json', is_flag=True, default=True)
 @click.pass_context
-def get(ctx, context, json_output):
+def status_get(ctx, context, json_output):
     base_url = ctx.obj.get('base_url')
     username = ctx.obj.get('username')
     password = ctx.obj.get('password')
@@ -92,11 +150,11 @@ def get(ctx, context, json_output):
 
     r = requests.get(status_url, auth=(username, password))
     if json_output:
-        click.echo(json.dumps(r.json(), indent=2))
+        print(json.dumps(r.json(), indent=2))
     else:
         for s in r.json():
             line = '[%s] %s - %s' % (s['context'], s['state'], s['description'])
-            click.echo(line)
+            print(line)
 
     if r.status_code != 200:
         exit(1)
